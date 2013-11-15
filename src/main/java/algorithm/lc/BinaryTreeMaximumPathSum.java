e47505e1cdb47b6f42250401c035b639f76b34a8
yexi <yexijiang@gmail.com>
2013-11-15 09:36:45 -0500

    improve BinaryTreeMaximumPathSum

--- src/main/java/algorithm/lc/BinaryTreeMaximumPathSum.java
+++ src/main/java/algorithm/lc/BinaryTreeMaximumPathSum.java
@@ -28,32 +28,27 @@ public class BinaryTreeMaximumPathSum {
 
   public class Solution {
     public int maxPathSum(TreeNode root) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
-      Wrapper max = new Wrapper(Integer.MIN_VALUE);
-      pathSum(root, max);
-      return max.val;
+          // IMPORTANT: Please reset any member data you declared, as
+          // the same Solution instance will be reused for each test case.
+      int[] max = new int[1];
+      max[0] = Integer.MIN_VALUE;
+      maxPath(root, max);
+      return max[0];
     }
-
-    // keep max while return max extensible path sum
-    private int pathSum(TreeNode node, Wrapper max) {
+    
+    /**
+     *  Store the maximize path and return the max extensible path.
+     */
+    private int maxPath(TreeNode node, int[] max) {
       if (node == null) {
         return 0;
       }
-      int l = pathSum(node.left, max);
-      int r = pathSum(node.right, max);
-      // update sub-solution optimum
-      max.val = Math.max(max.val, l + r + node.val);
-      int extensiblePathSum = Math.max(l, r) + node.val;
-      return Math.max(extensiblePathSum, 0);
-    }
-
-    public class Wrapper {
-      int val;
-
-      public Wrapper(int val) {
-        this.val = val;
-      }
+      int leftExt = maxPath(node.left, max);
+      int rightExt = maxPath(node.right, max);
+      
+      int maxExt = Math.max(Math.max(leftExt, rightExt), 0) + node.val;
+      max[0] = Math.max(Math.max(maxExt, max[0]), leftExt + rightExt + node.val);
+      return maxExt;
     }
   }
 
