7b5ae6391cd48bf252374f5f8786ebbaa1052c7c
yexi <yexijiang@gmail.com>
2014-08-27 15:08:56 -0400

    add burrows wheeler trnasform reverse

--- src/main/java/algorithm/basic/BurrowsWheelerTransform.java
+++ src/main/java/algorithm/basic/BurrowsWheelerTransform.java
@@ -23,4 +23,28 @@ public class BurrowsWheelerTransform {
 		return sb.toString();
 	}
 
+	public String reverse(String encoded, String eof) {
+		List<StringBuilder> table = new ArrayList<StringBuilder>();
+		for (int i = 0; i < encoded.length(); ++i) {
+			table.add(new StringBuilder());
+		}
+
+		for (int i = 0; i < encoded.length(); ++i) {
+			for (int j = 0; j < encoded.length(); ++j) {
+				table.get(j).insert(0, encoded.charAt(j));
+			}
+			Collections.sort(table, new Comparator<StringBuilder>() {
+				public int compare(StringBuilder sb1, StringBuilder sb2) {
+					return sb1.toString().compareTo(sb2.toString());
+				}
+			});
+		}
+		for (StringBuilder sb : table) {
+			if (sb.substring(sb.length() - 1).equals(eof)) {
+				return sb.toString().substring(0, sb.length() - 1);
+			}
+		}
+		return null;
+	}
+
 }
--- src/test/java/algorithm/basic/TestBurrowsWheelerTransform.java
+++ src/test/java/algorithm/basic/TestBurrowsWheelerTransform.java
@@ -1,7 +1,9 @@
 package algorithm.basic;
 
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 
 public class TestBurrowsWheelerTransform {
 
@@ -12,5 +14,21 @@ public class TestBurrowsWheelerTransform {
 		String s1 = "google";
 		String r1 = "elo#gog";
 		assertEquals(r1, bwt.bwt(s1, eof)); 
+	}
+
+	@Test
+	public void testReverse() {
+		BurrowsWheelerTransform bwt = new BurrowsWheelerTransform();
+		String eof = "#";
+		Random rnd = new Random();
+		for (int i = 0; i < 10; ++i) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < 10; ++j) {
+				char ch = (char)('a' + rnd.nextInt(26));
+				sb.append(ch);	
+			}
+			String encoded = bwt.bwt(sb.toString(), eof);
+			assertEquals(sb.toString(), bwt.reverse(encoded, eof)); 
+		}
 	}
 }
