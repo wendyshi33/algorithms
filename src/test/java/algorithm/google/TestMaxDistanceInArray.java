d923dc2f6138111e86422b441a564c808764bcf6
yexi <yexijiang@gmail.com>
2014-09-03 11:07:50 -0400

    add max distance in array

---
+++ src/test/java/algorithm/google/TestMaxDistanceInArray.java
@@ -0,0 +1,18 @@
+package algorithm.google;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestMaxDistanceInArray {
+
+	@Test
+	public void testMaxDistanceInArray() {
+		MaxDistanceInArray maxDist = new MaxDistanceInArray();
+		int[] arr1 = {1, 5, 2, 6, 8, 2, 1};
+		assertEquals(16, maxDist.maxDistanceInArray(arr1));
+		int[] arr2 = {6, 2, 1, 6, 8, 9, 1, 2, 3, 6, 1};
+		assertEquals(21, maxDist.maxDistanceInArray(arr2));
+		int[] arr3 = {};
+		assertEquals(0, maxDist.maxDistanceInArray(arr3));
+	}
+}
