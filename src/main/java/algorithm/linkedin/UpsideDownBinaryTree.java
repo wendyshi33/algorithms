c1b2312ee378c56bb740e9136cbb4e9e9e7f6d0e
yexi <yexijiang@gmail.com>
2014-09-05 21:15:40 -0400

    update

--- src/main/java/algorithm/linkedin/UpsideDownBinaryTree.java
+++ src/main/java/algorithm/linkedin/UpsideDownBinaryTree.java
@@ -1,5 +1,21 @@
 package algorithm.recursive;
 
+/**
+ * Given a binary tree where all the right nodes are leaves.
+ * Write a function to turn the tree upside down.
+ * e.g.
+ *      root -> 0                    0
+ *             / \                  / 
+ *            1   2                1 - 2
+ *           /                    /
+ *          3             ===>   3
+ *         / \                  /
+ *        4   5                4 - 5
+ *       / \                  /
+ *      6   7                6 - 7
+ *
+ * Spcae: O(1), Time: O(n)
+ */
 public class UpsideDownBinaryTree {
 
 	public static class TreeNode {
