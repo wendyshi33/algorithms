14874fad95c00e7474c7f77001a22fa1f27641fa
yexi <yexijiang@gmail.com>
2014-09-08 09:47:29 -0400

    add rotate image python version

---
+++ src/main/python/lc/rotate_image.py
@@ -0,0 +1,12 @@
+class Solution:
+	# @param matrix, a list of lists of integers
+	# @return a list of lists of integers
+	def rotate(self, matrix):
+		for layer in range(len(matrix) / 2):
+			end = len(matrix) - layer - 1
+			for start in range(layer, end):      
+				tmp = matrix[layer][start]
+				matrix[layer][start] = matrix[len(matrix) - 1 - start][layer]
+				matrix[len(matrix) - 1 - start][layer] = matrix[len(matrix) - 1 - layer][len(matrix) - 1 - start]
+				matrix[len(matrix) - 1 - layer][len(matrix) - 1 - start] = matrix[start][len(matrix) - 1 - layer]
+				matrix[start][len(matrix) - 1 - layer] = tmp
