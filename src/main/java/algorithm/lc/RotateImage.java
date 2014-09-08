5ee2d1c532102a87fffacf3ee18156b26dad6e36
yexi <yexijiang@gmail.com>
2014-09-08 09:36:01 -0400

    update

--- src/main/java/algorithm/lc/RotateImage.java
+++ src/main/java/algorithm/lc/RotateImage.java
@@ -8,33 +8,22 @@ package algorithm.lc;
  * 
  */
 // O(1) space, O(n^2) time
-public class RotateImage {
-
-  public class Solution {
-    // rotate the matrix from outer to inner, layer by layer
-    public void rotate(int[][] matrix) {
-      // Start typing your Java solution below
-      // DO NOT write main() function
-      for (int layer = 0; layer < matrix.length / 2; ++layer) {
-        int start = layer;
-        int end = matrix.length - layer - 1;
-        for (; start < end; ++start) {
-          int tmp = matrix[layer][start];
-          // replace top with left
-          matrix[layer][start] = matrix[matrix.length - start - 1][layer];
-          // replace left with bottom
-          matrix[matrix.length - start - 1][layer] = matrix[matrix.length
-              - layer - 1][matrix.length - start - 1];
-          // replace bottom with right
-          matrix[matrix.length - layer - 1][matrix.length - start - 1] = matrix[start][matrix.length
-              - layer - 1];
-          // replace right with top
-          matrix[start][matrix.length - layer - 1] = tmp;
-
-        }
+public class Solution {
+  public void rotate(int[][] matrix) {
+    for (int layer = 0; layer < matrix.length / 2; ++layer) {
+      int start = layer;
+      int end = matrix.length - layer - 1;
+      for (; start < end; ++start) {
+        int tmp = matrix[layer][start];
+				// replace upper with left
+        matrix[layer][start] = matrix[matrix.length - 1 - start][layer];
+				// replace left with bottom
+        matrix[matrix.length - 1 - start][layer] = matrix[matrix.length - 1 - layer][matrix.length - 1 - start];
+				// replace bottom with right
+        matrix[matrix.length - 1 - layer][matrix.length - 1 - start] = matrix[start][matrix.length - 1 - layer];
+				// replace right with upper
+        matrix[start][matrix.length - 1 - layer] = tmp;
       }
     }
-
   }
-
 }
