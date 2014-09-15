cefebcaf8bf7be1b14ecc05543963537ea0d1b3a
yexi <yexijiang@gmail.com>
2014-09-15 14:58:13 -0400

    update

--- src/main/java/algorithm/lc/InsertionSortList.java
+++ src/main/java/algorithm/lc/InsertionSortList.java
@@ -2,52 +2,44 @@ package algorithm.lc;
 
 public class InsertionSortList {
 
-  public class ListNode {
-    int val;
-    ListNode next;
+	public class ListNode {
+		int val;
+		ListNode next;
 
-    ListNode(int x) {
-      val = x;
-      next = null;
-    }
-  }
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
 
-  // O(1) space, O(n^2) time
-  public class Solution {
-    public ListNode insertionSortList(ListNode head) {
-      // IMPORTANT: Please reset any member data you declared, as
-      // the same Solution instance will be reused for each test case.
-      ListNode fakeHead = new ListNode(0);
-      fakeHead.next = head;
-      ListNode sorted = fakeHead;
+	// O(1) space, O(n^2) time
+	public class Solution {
+		public ListNode insertionSortList(ListNode head) {
+			if (head == null || head.next == null) {
+				return head;
+			}
+			ListNode newHead = new ListNode(0);
+			newHead.next = head;
+			ListNode sortedHead = head;
+			ListNode remainingHead = head.next; 
+			head.next = null;
 
-      while (sorted.next != null) {
-        ListNode prev = sorted;
-        ListNode cur = prev.next;
-        ListNode minNode = cur;
-        ListNode minPrev = prev;
-        
-        while (cur != null) { // find the min from the remaining list
-          if (cur.val < minNode.val) {
-            minNode = cur;
-            minPrev = prev;
-          }
-          prev = cur;
-          cur = cur.next;
-        }
-        
-        if (sorted.next != minNode) {
-          ListNode sortedNext = sorted.next;
-          sorted.next = minNode;
-          minPrev.next = minNode.next;
-          minNode.next = sortedNext;
-        }
-
-        sorted = sorted.next;
-      }
-
-      return fakeHead.next;
-    }
-  }
-
+			while (remainingHead != null) { // while there is node not sorted
+				ListNode sortedPrev = newHead;
+				while (sortedPrev.next != null && sortedPrev.next.val < remainingHead.val) {
+					sortedPrev = sortedPrev.next;
+				}
+				ListNode newRemainingHead = remainingHead.next;
+				if (sortedPrev.next == null) { // append to the tail of sorted list
+					remainingHead.next = null;
+					sortedPrev.next = remainingHead;
+				} else { // insert into sorted list
+					remainingHead.next = sortedPrev.next;
+					sortedPrev.next = remainingHead;
+				}
+				remainingHead = newRemainingHead;
+			}
+			return newHead.next;
+		}
+	}	
 }
