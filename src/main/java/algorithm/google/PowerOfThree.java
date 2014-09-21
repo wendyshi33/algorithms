507f1234410445138069a84f4adcb8e62d19dc82
yexi <yexijiang@gmail.com>
2014-09-21 14:57:26 -0400

    update

--- src/main/java/algorithm/google/PowerOfThree.java
+++ src/main/java/algorithm/google/PowerOfThree.java
@@ -3,17 +3,27 @@ package algorithm.google;
 /**
  *	Check whether a number is power of 3.
  *	
- *	O(log_3^N)
  */
 public class PowerOfThree {
 
 	public boolean isPowerOfThree(int num) {
-		if (num == 0 || num == 3) {
+		if (num == 0) {
 			return true;
-		} else if (num == 1 || num == 2) {
-			return false;
-		} else {
-			return num % 3 == 0 && isPowerOfThree(num / 3);
 		}
+		
+		do {
+			if (num % 3 != 0) {
+				return false;
+			}
+			int sum = 0;
+			int tmp = num;
+			while (tmp > 0) {
+				sum += tmp % 10;
+				tmp /= 10;	
+			}
+			num = sum / 3;
+		} while (num != 1); 
+
+		return true;
 	}
 }
