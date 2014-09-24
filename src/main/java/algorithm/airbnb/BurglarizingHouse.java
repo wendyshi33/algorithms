5e96d9b875b42ceecd2652f8cd65f003389af672
yexi <yexijiang@gmail.com>
2014-09-24 16:21:50 -0400

    update

--- src/main/java/algorithm/airbnb/BurglarizingHouse.java
+++ src/main/java/algorithm/airbnb/BurglarizingHouse.java
@@ -2,12 +2,20 @@ package algorithm.basic;
 
 import java.util.*;
 
+/**
+ *	A thief wants to steal money from the whole street, but he cannot steal two
+ *	consecutive houses as the house owners will immediately notify their neighbors.
+ *	Given the amount of money in each house, how can the thief maximize his swag?
+ *
+ *	Time complexity O(N), space Complexity O(N).
+ *
+ */
 public class BurglarizingHouse {
-	
+
 	public int bestValue(int[] houses) {
 		int[] values = new int[houses.length];		
 		Arrays.fill(values, -1);
-		
+
 		return best(houses, values, houses.length - 1);
 	}
 
